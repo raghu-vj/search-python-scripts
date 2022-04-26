75eac6b9d0d58196aeb264829aca8e3b967f3e9c
Raghunandan Jahagirdar <raghunandan.j@swiggy.in>
2022-04-26 21:03:34 +0530

    exclusion list

--- post_processing/scrape/scraper.py
+++ post_processing/scrape/scraper.py
@@ -4,8 +4,13 @@ import requests
 import json
 import datetime as dt
 
-HOST = "http://localhost:8080"
+from paramiko.client import SSHClient
+
 PROD_HOST = "http://voyager-search-service.swiggy.prod"
+
+client = SSHClient()
+client.load_system_host_keys()
+client.connect('sp.swiggy.co', port=4242, username="raghunandan.j")
 
 
 def read_from_file(file_name):
@@ -18,12 +23,25 @@ def append_to_file(file, text):
     file.write("\n")
 
 
+def curl_request(url,method,headers,payloads):
+    # construct the curl command from request
+    command = "curl -v -H {headers} {data} -X {method} {uri}"
+    data = " -d '" + payloads + "'"
+    header_list = ['"{0}: {1}"'.format(k, v) for k, v in headers.items()]
+    header = " -H ".join(header_list)
+    return command.format(method=method, headers=header, data=data, uri=url)
+
+def curl_request_v2(url,method,headers,payloads):
+    data = " -d '" + payloads + "'"
+    command = "curl --location --request POST '%s' --header 'swuid: 42705fa442b74c24' --header 'X-GRID: raghunandan' --header 'Content-Type: application/json' {data}" % url
+    return command.format(data=data, url=url)
+
 if __name__ == '__main__':
     location_data = read_from_file("data/location_data.csv")
     queries_data = read_from_file("data/low_mrr_queries.txt").split("\n")
-    base_url = HOST
-    if len(sys.argv) > 0:
-        base_url = PROD_HOST
+    base_url = PROD_HOST
+    # if len(sys.argv) > 0:
+    #     base_url = PROD_HOST
     hour_of_day = str(dt.datetime.now().hour)
     file_name = "generated/" + str(dt.datetime.now()) + "_Hour_" + hour_of_day + ".csv"
     file = open(file_name, "a")
@@ -38,30 +56,23 @@ if __name__ == '__main__':
                 location_within_city = location_data_split[3].strip()
                 city = location_data_split[4].strip()
                 url = (base_url + "/api/v3/search?maxCount=100&pageOffset=0&str=%s&entityType=RESTAURANT") % query
-                payload = json.dumps({
-                    "latLong": ("%s, %s" % (lat, long)),
-                    "submitAction": "DEFAULT_SUGGESTION",
-                    "trackingId": "1234590123",
-                    "trySpellCorrection": "false",
-                    "isAdsEnabled": "true",
-                    "context": {
-                        "isRadialCutEnabled": True,
-                        "filters": {}
-                    }
-                })
+                payload = "{\"latLong\":\"%s, %s\",\"submitAction\":\"DEFAULT_SUGGESTION\",\"trackingId\":\"1234590123\",\"trySpellCorrection\":\"false\",\"isAdsEnabled\":\"true\",\"context\":{\"isRadialCutEnabled\":true,\"filters\":{}}}" % (
+                lat, long)
                 headers = {
                     'swuid': '42705fa442b74c24',
                     'X-GRID': 'raghunandan',
                     'Content-Type': 'application/json'
                 }
-                response = requests.request("POST", url, headers=headers, data=payload)
-                json_data = json.loads(response.text)
+                request = curl_request_v2(url, "POST", headers, payload)
+                _, stdout, _ = client.exec_command(request)
+                json_data = json.loads(stdout.read())
                 append_to_file(file, "#### Location: " + location_within_city + " City: " + city_id + "(" + city + ")" + ", Hour of day: " + hour_of_day)
                 append_to_file(file, "" + "," + "Restaurant ID, Parent ID, Restaurant Name, Relevance")
                 for index, rest in enumerate(json_data['data']['searchResult']['RESTAURANT']):
                     if index > 19:
                         break
                     append_to_file(file, "" + "," + str(rest['id']) + "," + str(rest.get("parentId", "")) + "," + rest['name'])
-            except:
-                print
+            except Exception as e:
+                print(e)
+                pass
     file.close()
